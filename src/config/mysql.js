3dc9d0fa2b98f9a01dd4d5b4715473534263e94d
Ahmad Zaky <a.zaky32@gmail.com>
2023-05-17 18:40:44 +0700

    Add config file

---
+++ src/config/mysql.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DATABASE,
+});
+
+connection.connect((error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("you are now connected db mysql...");
+});
+
+module.exports = connection;
