7a2e0db2bd04fa112899e278218b4fc25087af43
Ahmad Zaky <a.zaky32@gmail.com>
2023-05-24 19:55:50 +0700

    change database url

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const urlDB =
-  "mysql://root:qCV0AhCF7NsmMDtmvWNm@containers-us-west-35.railway.app:6105/railway";
+  "mysql://root:DR97VHo22QVYiljwjcyF@containers-us-west-20.railway.app:6528/railway";
 // const connection = mysql.createConnection({
 //   host: process.env.HOST,
 //   user: process.env.DBUSER,
